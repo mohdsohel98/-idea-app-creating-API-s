7bb3cb714b348aeaa0c5da9b5ebe25e884bbab3e
Mohd.Sohel  mansoori <151987846+mohdsohel98@users.noreply.github.com>
2024-04-25 12:45:30 +0530

    Add files via upload

---
+++ ideas.model.js
@@ -0,0 +1,35 @@
+
+module.exports  = {
+1 : {
+ idea: 1,
+ idea_name : "'Greater idea',",
+ author_name:"shel khan",
+ idea_description  : " this is the final idea i ever i had ",
+
+},
+
+2 : {
+  idea: 2,
+  idea_name : "'Greater  second idea',",
+  author_name:"mummy",
+  idea_description  : " this is the final idea i ever i had ",
+ 
+ },
+
+ 3: {
+  idea: 3,
+  idea_name : "' third  idea',",
+  author_name:" sonbee",
+  idea_description  : " this is the final idea i ever i had ",
+ 
+ },
+
+ 4: {
+  idea: 4,
+  idea_name : "'last idea',",
+  author_name:"papa",
+  idea_description  : " this is the  last and final idea i ever i had ",
+ 
+ },
+
+}
